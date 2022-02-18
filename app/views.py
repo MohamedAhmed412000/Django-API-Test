e2c82a6f4aefb6a3228b525406cb4785f9db1e5a
Mohamed Ahmed <memoahmed412000@gmail.com>
2022-02-18 15:20:33 +0200

    Add API Data Files

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.http import Http404, JsonResponse, response
+from django.shortcuts import render
+
+# Create your views here.
+def welcome(request):
+    if (request.method == "GET"):
+        response = {
+            "message": "Hello, World!!"
+        }
+        return JsonResponse(response)
+    else:
+        return Http404
